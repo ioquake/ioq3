3ee7e5457537b34c12b5bbf17d0901bcaa829e89
Takkie <jas.mail.mob@gmail.com>
2013-12-08 07:38:04 +0100

    fix newweaponnum
    If we add more weapons this code needs to change accordingly

--- code/game/ai_dmq3.c
+++ code/game/ai_dmq3.c
@@ -1573,7 +1573,12 @@ void BotChooseWeapon(bot_state_t *bs) {
 		trap_EA_SelectWeapon(bs->client, bs->weaponnum);
 	}
 	else {
-		newweaponnum = trap_BotChooseBestFightWeapon(bs->ws, bs->inventory);
+//SlugRock
+/*		newweaponnum = trap_BotChooseBestFightWeapon(bs->ws, bs->inventory); */
+		newweaponnum = WP_RAILGUN; //SlugRock SCO We only have one weapon so let's choose it
+		if (newweaponnum != WP_RAILGUN); //SlugRock if weapon isn't the rg it is the rl
+			newweaponnum = WP_ROCKET_LAUNCHER;
+
 		if (bs->weaponnum != newweaponnum) bs->weaponchange_time = FloatTime();
 		bs->weaponnum = newweaponnum;
 		//BotAI_Print(PRT_MESSAGE, "bs->weaponnum = %d\n", bs->weaponnum);
