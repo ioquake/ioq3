0b2116c7813bd8b1d415da3e0e3903f674fbae02
Takkie <jas.mail.mob@gmail.com>
2013-12-21 07:33:52 +0100

    Change CenterPrint
    Change size and positioning of the "You fragged [player] for [nr.]
    place"

--- code/cgame/cg_event.c
+++ code/cgame/cg_event.c
@@ -226,7 +226,7 @@ static void CG_Obituary( entityState_t *ent ) {
 			CG_CenterPrint( s, SCREEN_HEIGHT * 0.30, BIGCHAR_WIDTH );
 		} 
 #else
-		CG_CenterPrint( s, SCREEN_HEIGHT * 0.30, BIGCHAR_WIDTH );
+		CG_CenterPrint( s, SCREEN_HEIGHT * 0.08, TINYCHAR_WIDTH );
 #endif
 
 		// print the text message as well
