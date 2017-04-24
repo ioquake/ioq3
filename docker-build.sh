b0137aabe275c1f9f638c444051be6be6ffffed0
Victor Roemer <victor@badsec.org>
2017-04-24 18:22:37 -0400

    Parallel linux/mingw32 builds Prebuilt docker container

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # Victor Roemer (wtfbbqhax) <victor@badsec.org>
 make -j 4
-PLATFORM=mingw32 make -j 4
 rm -rf build
 USE_CODEC_VORBIS=1 USE_FREETYPE=1 make -j 4
-PLATFORM=mingw32 USE_CODEC_VORBIS=1 USE_FREETYPE=1 make -j 4
 mkdir cbuild && cd cbuild && cmake .. && make -j 4 all game.qvm cgame.qvm ui.qvm
 
