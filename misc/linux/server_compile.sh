dbb8c35ffe4f4096e01e2b9ce199de950ecb53c6
Matt Palmer <mpalmer@hezmatt.org>
2017-11-22 13:48:07 +1100

    Go back to two parallel build jobs

--- misc/linux/server_compile.sh
+++ misc/linux/server_compile.sh
@@ -9,7 +9,7 @@ export USE_CODEC_OPUS="${USE_CODEC_OPUS:-1}"
 export USE_VOIP="${USE_VOIP:-1}"
 export COPYDIR="${COPYDIR:-~/ioquake3}"
 IOQ3REMOTE="${IOQ3REMOTE:-https://github.com/ioquake/ioq3.git}"
-MAKE_OPTS="${MAKE_OPTS:--j}"
+MAKE_OPTS="${MAKE_OPTS:--j2}"
 
 if ! [ -x "$(command -v git)" ] || ! [ -x "$(command -v make)" ]; then
         echo "This build script requires 'git' and 'make' to be installed." >&2
