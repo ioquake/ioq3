88a93df4aebb7a0a5263e3f3a22de52fa71c2381
Tim Angus <tim@ngus.net>
2025-07-16 11:05:47 +0100

    Update curl

--- misc/update-libs.sh
+++ misc/update-libs.sh
@@ -9,7 +9,9 @@ CODE="${ROOT}/code"
 prepare()
 {
     local URL="$1"
-    shift
+    local COMMAND="$2"
+    shift 2
+
     local EXCLUDE_PATTERNS=("$@")
     local FILENAME=$(basename ${URL})
 
@@ -26,15 +28,16 @@ prepare()
 
     (
         cd ${CODE}/${DIR}
-        [ -f ./configure ] && ./configure
+        [[ -n "$COMMAND" ]] && eval "$COMMAND"
         find . -type f ${EXCLUDE_ARGS[@]} -delete
         find . -type d -empty -delete
     )
 }
 
-prepare "https://downloads.xiph.org/releases/ogg/libogg-1.3.6.tar.gz" "\./\(include\|src\)/.*\.[ch]"
-prepare "https://downloads.xiph.org/releases/vorbis/libvorbis-1.3.7.tar.gz" "\./\(include\|lib\)/.*\.[ch]"
-prepare "https://downloads.xiph.org/releases/opus/opus-1.5.2.tar.gz" "\./\(celt\|include\|silk\|src\)/.*\.[ch]"
-prepare "https://downloads.xiph.org/releases/opus/opusfile-0.12.tar.gz" "\./\(include\|src\)/.*\.[ch]"
-prepare "https://zlib.net/zlib-1.3.1.tar.gz" "./[^/]*\.[ch]"
-prepare "https://www.ijg.org/files/jpegsrc.v9f.tar.gz" "./[^/]*\.[ch]"
+prepare "https://downloads.xiph.org/releases/ogg/libogg-1.3.6.tar.gz" "./configure" "\./\(include\|src\)/.*\.[ch]"
+prepare "https://downloads.xiph.org/releases/vorbis/libvorbis-1.3.7.tar.gz" "./configure" "\./\(include\|lib\)/.*\.[ch]"
+prepare "https://downloads.xiph.org/releases/opus/opus-1.5.2.tar.gz" "./configure" "\./\(celt\|include\|silk\|src\)/.*\.[ch]"
+prepare "https://downloads.xiph.org/releases/opus/opusfile-0.12.tar.gz" "./configure" "\./\(include\|src\)/.*\.[ch]"
+prepare "https://zlib.net/zlib-1.3.1.tar.gz" "./configure" "./[^/]*\.[ch]"
+prepare "https://www.ijg.org/files/jpegsrc.v9f.tar.gz" "./configure" "./[^/]*\.[ch]"
+prepare "https://curl.se/download/curl-8.15.0.tar.gz" "./configure --with-openssl" "\.*/include/.*\.h"
