c818196eb0ffd35e29a31760000ededba269a2e8
WofWca <wofwca@protonmail.com>
2025-09-13 18:56:45 +0400

    fix: shotgun not gibbing unless aiming at feet
    Closes https://github.com/ioquake/ioq3/issues/794.
    
    Note that now when shooting at two players
    standing behind each other,
    the second target will take less damage,
    because the dead body of the first player will absorb the pellets
    until it gets gibbed (that is, up to 4 pellets,
    see `GIB_HEALTH` and `DEFAULT_SHOTGUN_DAMAGE`).
    
    The purpose of the removed line is not entirely clear.
    Maybe it's just leftover code,
    because the height of a dead body is handled in `PM_CheckDuck`.
    
    See the issue description for more details.

--- code/game/g_combat.c
+++ code/game/g_combat.c
@@ -601,8 +601,6 @@ void player_die( gentity_t *self, gentity_t *inflictor, gentity_t *attacker, int
 
 	self->s.loopSound = 0;
 
-	self->r.maxs[2] = -8;
-
 	// don't allow respawn until the death anim is done
 	// g_forcerespawn may force spawning at some later time
 	self->client->respawnTime = level.time + 1700;
