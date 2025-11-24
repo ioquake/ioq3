0f5159fa8775361aa3a9685ae9a39a5d360f9e48
Lojjik Braughler <llbraughler@gmail.com>
2025-11-23 20:21:54 -0500

    Force BSD to use internal SDL

--- code/renderer/vulkan/vk_create_window_SDL.c
+++ code/renderer/vulkan/vk_create_window_SDL.c
@@ -32,16 +32,14 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "glConfig.h"
 
 
-#ifdef USE_INTERNAL_SDL_HEADERS
+#if defined(USE_INTERNAL_SDL_HEADERS) || defined(__OpenBSD__)
 #include "SDL.h"
 #include "SDL_syswm.h"
 #include "SDL_vulkan.h"
 #else
-
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_syswm.h>
 #include <SDL2/SDL_vulkan.h>
-
 #endif
 
 
