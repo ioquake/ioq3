16652c264938a1393301ca6cfa422cc3ef92ba79
Jordan Sissel <jls@semicomplete.com>
2026-07-19 21:04:59 -0700

    fix typo

--- code/cgame/cg_main.c
+++ code/cgame/cg_main.c
@@ -1952,7 +1952,7 @@ void CG_Init( int serverMessageNum, int serverCommandSequence, int clientNum ) {
 	trap_S_ClearLoopingSounds( qtrue );
 
 	//trap_Javascript("alert('Hello from Quake 3 game code')");
-	trap_Javascript("console.log('Greeetings from Quak3 game code')");
+	trap_Javascript("console.log('Greetings from Quake3 game code')");
 }
 
 /*
