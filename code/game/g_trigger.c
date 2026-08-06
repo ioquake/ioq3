aad58f01a2df9dd7f959f196f7b09bbd74d216ee
Jordan Sissel <jls@semicomplete.com>
2026-08-05 22:20:26 -0700

    remove a log and note the acme stuff is HF

--- code/game/g_trigger.c
+++ code/game/g_trigger.c
@@ -148,13 +148,13 @@ void trigger_push_touch (gentity_t *self, gentity_t *other, trace_t *trace ) {
 		return;
 	}
 
-  G_Printf("Jump pad in g_trigger - g_acme_jumppad == %d\n", g_acme_jumppad.integer);
+  //G_Printf("Jump pad in g_trigger - g_acme_jumppad == %d\n", g_acme_jumppad.integer);
 	BG_TouchJumpPad( &other->client->ps, &self->s );
 
 	if (g_acme_jumppad.integer > 0) {
 		// Mess with the jump pad trajectory...
 		acme = 1.0 + Q_crandom(&seed) * 0.1;
-		G_Printf ("Jump pad calculation x %0.4f\n", acme);
+		G_Printf ("HF: Acme Jump Pad x %0.4f\n", acme);
 		VectorScale(other->client->ps.velocity, acme, other->client->ps.velocity);
 	}
 }
